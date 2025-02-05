a47847bb099610c484d837676a365ceacf2af460
sonam-yoezer <forbusinesssjy999@gmail.com>
2025-02-05 16:49:43 +0600

    P01: Showcasing Home page and Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ProfileComponent},
+    {path: 'project', component: ProjectsComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'skill', component: SkillsComponent},
+    {path: 'contact', component: ContactComponent}
+    
+];
